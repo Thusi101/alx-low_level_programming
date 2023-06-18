b320274c818354974259f0687990a334308f7c00
Your Name <moraseroka10@gmail.com>
2023-06-18 15:45:24 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z' || c <= 'Z')
+	{
+		putchar(c);
+		c++;
+			if (c > 'z')
+			{
+				c = 'A';
+			}
+	}
+
+	putchar('\n');
+
+	return 0;
+}
