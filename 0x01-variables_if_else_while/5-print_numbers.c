a57b5185100f487f307a4f9632d33825f7efc718
Your Name <moraseroka10@gmail.com>
2023-06-18 17:37:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Must be Zero 0 (Successfully)
+*/
+
+int main(void)
+
+{
+
+	int j;
+	for (j = 0; j < 10; j++)
+		printf("%d", j);
+	printf("\n");
+
+	return (0);
+
+}
