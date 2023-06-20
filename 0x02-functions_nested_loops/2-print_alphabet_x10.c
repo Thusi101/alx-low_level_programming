59698b1664380c18d650715f9fc8e7ce1e7a0daf
Your Name <moraseroka10@gmail.com>
2023-06-20 09:35:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,19 @@
 
 /**
  * print_alphabet_x10 - prints alpha 10 times
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 
 {
-	int ten;
+	int ten = 0;
 
 	char pha;
 
 	for (ten = 0; ten <= 9; ten++)
 	{
 	for (pha = 'a'; pha <= 'z'; pha++)
-		_putchat(la);
+		_putchat(pha);
 	}
 }
