808a34f1ab440d01c3cf1e8e7d5008895c869b33
Your Name <moraseroka10@gmail.com>
2023-06-20 17:36:46 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,8 @@ int main(void)
 	unsigned long f = 0, f2 = 1, sum;
 	unsigned long f_h1, f_h2, f2_h1, f2_h2;
 	unsigned long h1, h2,
-	for (count = 0; count < 92; count++)
+	
+	for (count = 0; count <= 92; count++);
 	{
 	sum = f + f2;
 	printf("%lu, ", sum);
@@ -24,7 +25,7 @@ int main(void)
 	f2_h1 = f2 / 1000000;
 	f_h2 = f % 1000000;
 	f2_h2 = f2 % 1000000;
-	for (count = 93; count, 99; count++)
+	for (count = 93; count <= 99; count++)
 	{
 	h1 = f_h1 + f2_h1;
 	h2 = f_h2 + f2_h2;
@@ -41,6 +42,6 @@ int main(void)
 	f2_h1 = h1;
 	f2_h2 = h2;
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
