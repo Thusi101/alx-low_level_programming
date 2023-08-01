c8fd5eba4d2c5eb258c7383cab7945aa45b14689
Thusi101 <moraseroka10@gmail.com>
2023-08-01 14:42:20 -0700

    printlist

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,8 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	if (h == NULL)
-		return (0);
-	printf("%d\n", h->n);
-	return (1 + print_listint(h->next));
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
