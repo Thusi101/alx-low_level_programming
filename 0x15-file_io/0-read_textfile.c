38e22033b4321bef5264126a2a134bab1c442c3e
Thusi101 <moraseroka10@gmail.com>
2023-08-07 16:43:14 -0700

    Wilde

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a txt file & prints to POSIX std output
+ * @filename: name of file to be read
+ * @letters: number of letters to be read & printed
+ *
+ * Return: actual number of letters that were read & printed, or 0 if an error
+ * occurred or filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (n_written);
+}
