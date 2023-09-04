e86e7cbb7bed2b1cd6fe0949a87691c0f40c68d2
Thusi101 <moraseroka10@gmail.com>
2023-09-04 16:52:40 -0700

    txtread

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,19 +15,39 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int fd;
 
-	fd = open(filename, O_RDONLY);
+	ssize_t _read, _write;
+
+	char *buffer;
+	
+	if (!filename)
+	{
+		return (0);
+	}
+	fd = open(filebname, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	_read = read(fd, buffer, letters);
+	if (read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	_write = write(STDOUT_FILENO, buffer, read);
+	if (write == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 	close(fd);
-	return (w);
+	return (_read);
 }
