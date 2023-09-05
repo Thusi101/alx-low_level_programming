985013cd1244f2b5f1bbfe17a00a832a0474e2b0
Thusi101 <moraseroka10@gmail.com>
2023-09-04 17:00:01 -0700

    handles

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: a string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t n_written;
+	size_t len;
+	
+	if (filename == NULL)
+		return (-1);
+	
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++);
+		n_written = write(fd, text_content, len);
+		if (n_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	
+	close(fd);
+	return (1);
+}
