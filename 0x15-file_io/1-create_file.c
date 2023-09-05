af85cf78677df67d523497cc75ce6b988618a5ba
Thusi101 <moraseroka10@gmail.com>
2023-09-05 10:19:05 -0700

    main

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,17 +16,18 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t n_written;
 	size_t len;
-	
+
 	if (filename == NULL)
 		return (-1);
-	
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++);
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
 		n_written = write(fd, text_content, len);
 		if (n_written == -1)
 		{
@@ -34,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	
+
 	close(fd);
 	return (1);
 }
