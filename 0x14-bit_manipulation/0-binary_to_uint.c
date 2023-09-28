ddcad33f3d8841d2b78cf249a08b651166d72aeb
Thusi <65259676+Thusi101@users.noreply.github.com>
2023-09-28 13:43:47 +0200

    0.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: The binary number as a string.
+ *
+ * Return: The unsigned int representation of the binary number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		num <<= 1;
+
+		if (b[i] == '1')
+			num += 1;
+	}
+
+	return (num);
+}
