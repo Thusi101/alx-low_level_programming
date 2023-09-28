9487033b36c8e618b53d4430484f066e120f66a4
Thusi101 <moraseroka10@gmail.com>
2023-09-28 14:59:20 -0700

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,5 +9,5 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    return (index > 63) ? -1 : (*n = ((1UL << index) | *n), 1);
+	return ((index > 63) ? -1 : (*n = ((1UL << index) | *n), 1));
 }
