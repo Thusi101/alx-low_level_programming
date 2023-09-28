b3d006190565bc32291fdc737d8cba7492b22258
Thusi <65259676+Thusi101@users.noreply.github.com>
2023-09-28 13:54:32 +0200

    but

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int i;
+	int i, binary;
 
 	if (!b)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
+		;
+	for (i--, binary = 1; i >= 0; i--, binary *= 2)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
