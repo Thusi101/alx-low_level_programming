fc9b7d24b8a601b29deffc884208a8a38223f765
Thusi101 <moraseroka10@gmail.com>
2024-01-29 08:20:19 -0800

    using shared library in python that contains c functions

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * divide - divides two integers
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Division of two integers
+ */
+int divide(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mul - multiply two integers
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
